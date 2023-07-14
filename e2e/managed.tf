a7dc7898cfabddd3fa52a82e7bea750ebb5c1aea
Andrew Reed <w.andrew.reed@gmail.com>
2023-07-14 10:02:17 -0500

    Add smallstep_attestation_authority (#6)

--- e2e/managed.tf
+++ e2e/managed.tf
@@ -1,12 +1,19 @@
 
 resource "smallstep_collection" "tpms" {
-	slug = "tpms"
+  slug = "tpms"
 }
 
 resource "smallstep_collection_instance" "server1" {
-	id = "urn:ek:sha256:RAzbOveN1Y45fYubuTxu5jOXWtOK1HbfZ7yHjBuWlyE="
-	data = "{}"
-	collection_slug = smallstep_collection.tpms.slug
-	depends_on = [smallstep_collection.tpms]
+  id = "urn:ek:sha256:RAzbOveN1Y45fYubuTxu5jOXWtOK1HbfZ7yHjBuWlyE="
+  data = "{}"
+  collection_slug = smallstep_collection.tpms.slug
+  depends_on = [smallstep_collection.tpms]
+}
+
+resource "smallstep_attestation_authority" "aa" {
+  name = "foo"
+  catalog = smallstep_collection.tpms.slug
+  attestor_roots = file("${path.module}/ca.crt")
+  depends_on = [smallstep_collection.tpms]
 }
 
